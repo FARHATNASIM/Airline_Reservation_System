190c1527c641986e949c289b50d6787e4af0a636
FARHAT NASIM <118013560+FARHATNASIM@users.noreply.github.com>
2023-05-25 19:09:03 +0530

    init

---
+++ Airline_Reservation_System/Airline_Reservation_System/src/main/java/com/mycompany/airline_reservation_system/custID.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.airline_reservation_system;
+
+/**
+ *
+ * @author 91909
+ */
+class custID {
+
+    static void setText(String cS001) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
